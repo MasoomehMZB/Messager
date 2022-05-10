7f2cefc9c85053f3615a125b9213928b69dabda7
Negin <masoomeh.mzb@gmail.com>
2022-05-10 12:58:52 +0430

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("\\view\\LoginRegisterPageView.fxml"));
+        fxmlLoader.load();
+        Scene scene = new Scene(fxmlLoader.getRoot());
+        primaryStage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("\\view\\LoginRegisterView.css").toExternalForm());
+        primaryStage.show();
+
     }
 }
